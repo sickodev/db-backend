08991d93e4be68be4eb2fe395b7587e0c42ba192
sickodev <kbishwa03kalyan@gmail.com>
2023-07-04 12:19:07 +0530

    added schema and post

---
+++ model/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+    },
+    email: {
+        required: true,
+        type: String,
+    },
+    uid: {
+        required: true,
+        type: String,
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const Model = require("./model/model");
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 
+const uri = process.env.DATABASE_URL;
+
+mongoose.connect(uri);
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.log(error);
+});
+
+db.once("connected", () => {
+    console.log("Connected to DB");
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        email: req.body.email,
+        uid: req.body.uid,
+    });
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 app.listen(5000, () => {
